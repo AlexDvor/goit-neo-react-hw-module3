c1cab7ba2f162f2dec07b9e6e46abe6a92b30b85
AlexDvor <alexmatvichuk@gmail.com>
2025-12-14 10:47:34 +0100

    ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+
+import { useState } from 'react';
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 
-const contact = [
+const dataUsers = [
 	{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
 	{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
 	{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -11,12 +13,19 @@ const contact = [
 ];
 
 function App() {
+	const [contacts, setContacts] = useState(dataUsers);
+	const [searchField, setSearchField] = useState('');
+
+	const handleSearchChange = e => {
+		setSearchField(e.target.value);
+	};
+
 	return (
 		<div className='container'>
 			<h1>Phonebook</h1>
 			<ContactForm />
-			<SearchBox />
-			<ContactList contacts={contact} />
+			<SearchBox searchValue={searchField} handleChange={handleSearchChange} />
+			<ContactList contacts={contacts} />
 		</div>
 	);
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,18 @@
-const SearchBox = () => {
-	return <div>SearchBox</div>;
+import s from './SearchBox.module.css';
+
+const SearchBox = ({ searchValue, handleChange }) => {
+	return (
+		<div className={s.thumb}>
+			<label htmlFor='search'>Find contacts by name</label>
+			<input
+				type='text'
+				name='search'
+				id='search'
+				value={searchValue}
+				onChange={handleChange}
+			></input>
+		</div>
+	);
 };
 
 export default SearchBox;
