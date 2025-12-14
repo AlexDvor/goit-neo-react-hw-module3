d6002c0eda5154e339b161ade2835cb7335e066c
AlexDvor <alexmatvichuk@gmail.com>
2025-12-14 12:12:08 +0100

    add regex

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { nanoid } from 'nanoid';
+import { CardSchema } from '../validation';
 
 import s from './ContactForm.module.css';
 
@@ -11,24 +12,32 @@ const initialFieldsValue = {
 const ContactForm = ({ handleAdd }) => {
 	const handleSubmit = (value, action) => {
 		const newItem = { ...value, id: nanoid() };
-
 		handleAdd(newItem);
 		action.resetForm();
 	};
 
 	return (
 		<>
-			<Formik initialValues={initialFieldsValue} onSubmit={handleSubmit}>
+			<Formik
+				initialValues={initialFieldsValue}
+				validationSchema={CardSchema}
+				onSubmit={handleSubmit}
+			>
 				<Form className={s.formWrap}>
 					<div className={s.fieldBox}>
 						<label htmlFor='name'>Name</label>
-						<Field name='name' type='text' id='name' />
-						{/* <ErrorMessage name='name' component='div' /> */}
+						<Field name='name' type='text' id='name' placeholder='Pedro' />
+						<ErrorMessage name='name' component='p' className={s.error} />
 					</div>
 					<div className={s.fieldBox}>
 						<label htmlFor='number'>Number</label>
-						<Field name='number' type='phone' id='number' />
-						{/* <ErrorMessage name='phone' component='div' /> */}
+						<Field
+							name='number'
+							type='text'
+							id='number'
+							placeholder='631-26-86'
+						/>
+						<ErrorMessage name='number' component='p' className={s.error} />
 					</div>
 
 					<button type='submit'>Add contact</button>
